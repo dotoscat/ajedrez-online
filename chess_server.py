0427971e0fe6092764ce31c6cdf5f84681c056ba
Oscar Triano dotoscat <user@example.com>
2018-04-12 17:59:25 +0200

    La ip se puede configurar con valor por defecto

--- chess_server.py
+++ chess_server.py
@@ -1,10 +1,14 @@
 import signal
 import socket
+import argparse
 
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 ip_list = socket.gethostbyname_ex(socket.gethostname())[2]
-ip = ip_list.pop()
+
+parse = argparse.ArgumentParser()
+parse.add_argument("--ip", default=ip_list.pop(), help="ipv4")
+args = parse.parse_args()
 
 if __name__ == "__main__":
-    print("Listen from", ip)
+    print("Listen from", args.ip)
