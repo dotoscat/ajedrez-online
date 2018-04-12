be11ce08007ebe539400ef7c5c850e5ff1ef45f2
Oscar Triano dotoscat <user@example.com>
2018-04-12 18:26:05 +0200

    Implementar un servidor basico que escucha

--- chess_server.py
+++ chess_server.py
@@ -1,10 +1,30 @@
 import signal
 import socket
 import argparse
+import selectors
 
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
+class Server:
+    def __init__(self, ip, port):
+        self.host = (ip, port)
+        self.listener = socket.socket()
+        self.listener.setblocking(False)
+        self.listener.bind(self.host)
+        self.selector = selectors.DefaultSelector()
+        self.selector.register(self.listener, selectors.EVENT_READ, self.accept)
 
+    def accept(self, socket):
+        pass
+
+    def run(self):
+        while True:
+            events = self.selector.select()
+            for key, events in events:
+                print(key, events)
+
+    def __del__(self):
+        self.selector.close()
 
 def main():
     ip_list = socket.gethostbyname_ex(socket.gethostname())[2]
