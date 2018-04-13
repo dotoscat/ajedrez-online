faef335855b9c3a25b8cb33c1ec05fbb43535813
Oscar Triano dotoscat <user@example.com>
2018-04-13 13:38:08 +0200

    Escribir un cliente basico

--- chess_client.py
+++ chess_client.py
@@ -1,7 +1,25 @@
 import argparse
 import logging
+import selectors
+import signal
+import socket
+import errno
+import os
 
+signal.signal(signal.SIGINT, signal.SIG_DFL)
 logging.basicConfig(format="%(module)s:%(levelname)s:%(message)s", level=logging.DEBUG)
+
+class Client:
+    def __init__(self, ip, port):
+        self.selectors = selectors.DefaultSelector()
+        self.conn = socket.socket()
+        self.playing = False
+        err = self.conn.connect_ex((ip, port))
+        if err:
+            raise RuntimeError(os.strerror(err))
+
+    def __del__(self):
+        self.selectors.close()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
