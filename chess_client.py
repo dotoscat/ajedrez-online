b4bcae418951051a30dfe746bb0a28f0798da44c
Oscar Triano dotoscat <user@example.com>
2018-04-14 15:29:43 +0200

    Esperando entrada, y salida, del tablero

--- chess_client.py
+++ chess_client.py
@@ -6,6 +6,7 @@ import socket
 import errno
 import os
 from chessasir import protocol
+import chessasir
 
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 logging.basicConfig(format="%(module)s:%(levelname)s:%(message)s", level=logging.DEBUG)
@@ -17,17 +18,27 @@ class Client:
         self.host = (ip, port)
         self.playing = False
         self.color = None
+        self.turn = False
         self.selectors.register(self.conn, selectors.EVENT_READ, self.read)
 
     def read(self, conn):
         data = conn.recv(1024)
         logging.info("from server: {}".format(data))
         command = protocol.get_command(data)
+        print("commang", command)
         if command == protocol.STARTGAME:
             self.start_game(data)
 
     def start_game(self, data):
         command, color = protocol.startgame.unpack(data)
+        self.color = color
+        if color == chessasir.WHITE:
+            self.turn = True
+            print("You are white")
+        else:
+            self.turn = False
+            print("You are black")
+        self.playing = True
 
     def __del__(self):
         self.selectors.close()
@@ -40,7 +51,11 @@ class Client:
             return
         while True:
             if not self.playing:
-                print("Esperando jugador...")
+                print("Waiting a player to join...")
+            elif self.turn:
+                print("Enter your input")
+            else:
+                print("Waiting for your rival's move...")
             events = self.selectors.select()
             for key, events in events:
                 key.data(key.fileobj)
--- chessasir/protocol.py
+++ chessasir/protocol.py
@@ -7,4 +7,4 @@ command = struct.Struct("!B")
 startgame = struct.Struct("!BB")
 
 def get_command(buffer):
-    return command.unpack_from(buffer)
+    return command.unpack_from(buffer)[0]
