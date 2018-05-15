9b65cad1be2a781c8f67d5776511f4f86ebb8c55
Oscar Triano dotoscat <user@example.com>
2018-05-15 11:12:31 +0200

    Enviar mensaje de vuelta OKMOVE

--- chess-client/game.js
+++ chess-client/game.js
@@ -18,20 +18,35 @@ class Game{
             case "PLAYERQUITS":
                 this.playerQuits(message);
             break;
+            case "OKMOVE":
+                this.OKMove(message);
+            break;
         }
     }
 
     startGame(message){
         this.playing = true;
         this.boardView.assignedColor = message.color;
-        messages.add("Game start! You are " + message.color + ".");
+        this.messages.add("Game start! You are " + message.color + ".");
         this.boardView.block = message.color !== "WHITE";
     }
 
     playerQuits(message){
         this.playing = false;
-        messages.add("Player " + message.color + " quits from the match.");
-        messages.add("Waiting a player...");
+        this.messages.add("Player " + message.color + " quits from the match.");
+        this.messages.add("Waiting a player...");
+    }
+
+    OKMove(message){
+        const color = message.color;
+        const san = message.san;
+        if (color === 'WHITE'){
+            const turn = message.turn;
+            this.messages.add(`${turn}. ${san}`);         
+        }else{
+            this.messages.addToLast(` ${san}`);
+        }
+        this.boardView.block = true;
     }
 
 }
